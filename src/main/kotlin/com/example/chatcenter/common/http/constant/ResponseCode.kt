7040927c16c0aeebdbd351af536ecdf7e3e5a556
taeyeonKim <tae8753@gmail.com>
2024-02-15 18:14:42 +0900

    chat-center init

--- src/main/kotlin/com/example/chatcenter/common/http/constant/ResponseCode.kt
+++ src/main/kotlin/com/example/chatcenter/common/http/constant/ResponseCode.kt
@@ -5,7 +5,11 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 
 
-enum class ResponseCode(private val code: String, private val message: String) {
+enum class ResponseCode(
+    private val code: String,
+    private val message: String,
+    private val statusCode: Int = 200,
+) {
 
     // SUCCESS
     SUCCESS("SUCCESS", "success."),
@@ -13,13 +17,13 @@ enum class ResponseCode(private val code: String, private val message: String) {
     // ERROR
 
     // USER
-    INVALID_TOKEN("INVALID_TOKEN_ERROR", "invalid token error occurred."),
     WRONG_PASSWORD_ERROR("WRONG_PASSWORD_ERROR", "wrong password error occurred."),
     ALREADY_LOGOUT_ERROR("ALREADY_LOGOUT_ERROR", "already logout error occurred."),
 
     // AUTH
-    AUTH_ERROR("AUTH_ERROR", "auth error occurred."),
-    ACCESS_DENIED_ERROR("ACCESS_DENIED_ERROR", "access denied error occurred."),
+    AUTH_ERROR("AUTH_ERROR", "auth error occurred.", 401),
+    INVALID_TOKEN("INVALID_TOKEN_ERROR", "invalid token error occurred.", 401),
+    ACCESS_DENIED_ERROR("ACCESS_DENIED_ERROR", "access denied error occurred.", 403),
 
     // MEMBER
     EXIST_MEMBER("EXIST_MEMBER", "already member error occurred."),
@@ -37,25 +41,35 @@ enum class ResponseCode(private val code: String, private val message: String) {
 
     fun toResponse(): ResponseEntity<Response> {
         val response = Response(this.code, this.message)
-        return ResponseEntity.ok(response)
+        return ResponseEntity
+            .status(this.statusCode)
+            .body(response)
     }
 
     fun toResponse(body: Any): ResponseEntity<Response> {
         val response = Response(this.code, this.message, body)
-        return ResponseEntity.ok(response)
+        return ResponseEntity
+            .status(this.statusCode)
+            .body(response)
     }
 
     fun toResponse(headers: HttpHeaders): ResponseEntity<Response> {
         val response = Response(this.code, this.message)
-        return ResponseEntity.ok().headers(headers).body(response)
+        return ResponseEntity
+            .status(this.statusCode)
+            .headers(headers)
+            .body(response)
     }
 
     fun toResponse(headers: HttpHeaders, body: Any): ResponseEntity<Response> {
         val response = Response(this.code, this.message, body)
-        return ResponseEntity.ok().headers(headers).body(response)
+        return ResponseEntity
+            .status(this.statusCode)
+            .headers(headers)
+            .body(response)
     }
 
     fun print(): String {
-        return "ResponseCode - code : $code, message: $message"
+        return "ResponseCode - status: $statusCode, code : $code, message: $message"
     }
 }
